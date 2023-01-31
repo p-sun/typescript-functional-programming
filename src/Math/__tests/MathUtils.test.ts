36bf3de08c4094ad420a6ab669ce6d0d7fdbc025
Paige Sun <paige.sun.dev@gmail.com>
2023-01-31 14:53:55 -0800

    setup: Create the most minimal template for creating a NodeJS website.

--- src/Math/__tests/MathUtils.test.ts
+++ src/Math/__tests/MathUtils.test.ts
@@ -2,6 +2,6 @@ import { clamp } from '../MathUtils';
 
 describe('test intial game state', () => {
   it('should wait for spacebar keypress before starting the game', () => {
-    expect(clamp(100, { min: 0, max: 30 })).toBe(30);
+    expect(clamp(100, 0, 30)).toBe(30);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
-import HTMLCanvas from './Canvas/HTMLCanvas';
-import { ICanvas } from './Canvas/ICanvas';
-import Vec2 from './Math/Vec2';
-import Template from './Playgrounds/Template';
-
-const Playgrounds = {
-  DrawTemplate: (canvas: ICanvas) => {
-    const size = new Vec2(480, 480);
-    canvas.size = size;
-    return new Template();
-  },
-};
-
-const appRoot = document.getElementById('root');
-if (appRoot) {
-  const canvas = HTMLCanvas.createInRootElement(appRoot!);
-  const playground = Playgrounds.DrawTemplate(canvas);
-  playground.run(canvas);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const para = document.createElement('p');
+  const node = document.createTextNode('Hello World');
+  para.appendChild(node);
+  rootElement.appendChild(para);
 }
