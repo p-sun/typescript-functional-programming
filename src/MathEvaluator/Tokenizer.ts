374fb5737fcb66f03a64897a736a053e25a03b8b
Paige Sun <paige.sun.dev@gmail.com>
2023-02-01 13:24:12 -0800

    feat: Pass `'3*4+5'` for tokenizer, by adding a generic `charParser` function that only parses chars from a set.

--- src/MathEvaluator/Tokenizer.ts
+++ src/MathEvaluator/Tokenizer.ts
@@ -24,6 +24,52 @@ export class TextBuffer {
   }
 }
 
+class ParseResult<T> {
+  constructor(
+    public readonly data:
+      | { tag: 'value'; value: T }
+      | { tag: 'error'; message: string }
+  ) {}
+
+  toString() {
+    return JSON.stringify(this.data);
+  }
+
+  static value<T>(value: T) {
+    return new ParseResult({ tag: 'value', value });
+  }
+
+  static error<T>(message: string) {
+    return new ParseResult<T>({ tag: 'error', message });
+  }
+}
+
+type Parser<T> = (buffer: TextBuffer) => ParseResult<T>;
+
+function parseChars(chars: Set<string>): Parser<string> {
+  return (buffer) => {
+    const l = buffer.get();
+    if (chars.has(l)) {
+      return ParseResult.value(l);
+    } else {
+      return ParseResult.error(`Expected letter '${chars}' but got '${l}'`);
+    }
+  };
+}
+
 export default function tokenizer(contents: string): Token[] {
-  return contents.split(' ');
+  let buffer = new TextBuffer(contents);
+  let tokens = new Array<string>();
+  const charParser = parseChars(new Set(['3', '*', '4', '+', '5']));
+
+  let result: ParseResult<string> | undefined;
+
+  do {
+    result = charParser(buffer);
+    if (result.data.tag === 'value') {
+      tokens.push(result.data.value);
+    }
+  } while (result.data.tag !== 'error');
+
+  return tokens;
 }
