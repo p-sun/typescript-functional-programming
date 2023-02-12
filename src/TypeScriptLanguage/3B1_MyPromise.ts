ff5df1f621a4e4dab37a4d693e7f05bf9184d9e0
Paige Sun <paige.sun.dev@gmail.com>
2023-02-11 16:54:55 -0800

    refactor: Add Wafflecopters' refactors.

--- src/TypeScriptLanguage/3B1_MyPromise.ts
+++ src/TypeScriptLanguage/3B1_MyPromise.ts
@@ -2,123 +2,30 @@
 /*                      Implement a Promise from Scratch                      */
 /* -------------------------------------------------------------------------- */
 
-// then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
+// This is similar to type from JS Promise:
+//   then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
 type ResolveListener<T, S> = (value: T) => S | MyPromise<S>;
-// catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): Promise<T | TResult>;
+
+// This is similar to type from JS Promise:
+//   catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): Promise<T | TResult>;
 type RejectListener<S> = (err: unknown) => S | MyPromise<S>;
 
+type PromiseResult<T> =
+  | { tag: 'value'; value: T }
+  | { tag: 'error'; error: unknown };
+
+type PromiseState<T> = PromiseResult<T> | { tag: 'pending' };
+
 export class MyPromise<T> {
-  private result:
-    | { tag: 'value'; value: T }
-    | { tag: 'error'; error: unknown }
-    | { tag: 'pending' } = { tag: 'pending' };
+  private result: PromiseState<T> = { tag: 'pending' };
 
   private resolveListeners: ResolveListener<T, unknown>[] = [];
   private rejectListeners: RejectListener<unknown>[] = [];
 
-  constructor(
-    fn: (
-      resolve: (value: T | MyPromise<T>) => void,
-      reject: (error: unknown) => void
-    ) => void
-  ) {
-    fn(
-      // Define resolve function
-      (value) => {
-        if (value instanceof MyPromise) {
-          value
-            .then((awaitedVal: T) => {
-              this._resolveListeners(awaitedVal);
-            })
-            .catch((awaitedError) => {
-              this._rejectListeners(awaitedError);
-            });
-        } else {
-          this._resolveListeners(value);
-        }
-      },
-      // Define reject function
-      (error) => {
-        if (error instanceof MyPromise) {
-          error
-            .then((awaitedVal: T) => {
-              this._resolveListeners(awaitedVal);
-            })
-            .catch((awaitedError) => {
-              this._rejectListeners(awaitedError);
-            });
-        } else {
-          this._rejectListeners(error);
-        }
-      }
-    );
-  }
-
-  then<S>(onfulfilled: ResolveListener<T, S>): MyPromise<S> {
-    return new MyPromise<S>((resolve, reject) => {
-      if (this.result.tag === 'pending') {
-        this.resolveListeners.push((value: T) => {
-          resolve(onfulfilled(value));
-        });
-        this.rejectListeners.push((error: unknown) => {
-          reject(error);
-        });
-      } else if (this.result.tag === 'error') {
-        reject(this.result.error);
-      } else {
-        resolve(onfulfilled(this.result.value));
-      }
-    });
-  }
-
-  catch<S>(onrejected: RejectListener<S>): MyPromise<S | T> {
-    return new MyPromise((resolve, reject) => {
-      if (this.result.tag === 'pending') {
-        this.resolveListeners.push((value: T) => {
-          resolve(value);
-        });
-        this.rejectListeners.push((error: unknown) => {
-          reject(onrejected(error));
-        });
-      } else if (this.result.tag === 'error') {
-        reject(onrejected(this.result.error));
-      } else {
-        resolve(this.result.value);
-      }
-    });
-  }
-
-  _resolveListeners(value: T) {
-    this.result = { tag: 'value', value };
-
-    for (const listener of this.resolveListeners) {
-      listener(value);
-    }
-  }
-
-  _rejectListeners(error: unknown) {
-    this.result = { tag: 'error', error };
-
-    for (const listener of this.rejectListeners) {
-      listener(error);
-    }
-  }
-}
-
-class PromiseBuilder {
-  // A promise that will resolve when
-  // 1) value is type T
-  // 2) value is a promise, and that promise got resolved
   static resolve<T>(value: T | MyPromise<T>): MyPromise<T> {
     return new MyPromise<T>((resolve, reject) => {
       if (value instanceof MyPromise) {
-        value
-          .then((awaitedVal) => {
-            resolve(awaitedVal);
-          })
-          .catch((error: unknown) => {
-            reject(error);
-          });
+        value.then(resolve).catch(reject);
       } else {
         resolve(value);
       }
@@ -129,5 +36,93 @@ class PromiseBuilder {
     return new MyPromise((resolve, reject) => {
       reject(error);
     });
+  }
+
+  constructor(
+    fn: (
+      resolve: (value: T | MyPromise<T>) => void,
+      reject: (error: unknown) => void
+    ) => void
+  ) {
+    fn(
+      this.applyOrAwait(this.resolveAndAnnounce.bind(this)),
+      this.applyOrAwait(this.rejectAndAnnounce.bind(this))
+    );
+  }
+
+  then<S>(onfulfilled: ResolveListener<T, S>): MyPromise<S> {
+    return new MyPromise<S>((_resolve, reject) => {
+      this.run((t: T) => _resolve(onfulfilled(t)), reject);
+    });
+  }
+
+  catch<S>(onrejected: RejectListener<S>): MyPromise<S | T> {
+    return new MyPromise((resolve, _reject) => {
+      this.run(resolve, (e: unknown) => _reject(onrejected(e)));
+    });
+  }
+
+  private applyOrAwait<S>(
+    fn: (s: S) => void
+  ): (value: MyPromise<T> | S) => void {
+    return (value) => {
+      if (value instanceof MyPromise) {
+        value
+          .then(this.resolveAndAnnounce.bind(this))
+          .catch(this.rejectAndAnnounce.bind(this));
+      } else {
+        fn(value);
+      }
+    };
+  }
+
+  private run(resolve: (t: T) => void, reject: (e: unknown) => void) {
+    if (this.result.tag === 'pending') {
+      this.resolveListeners.push((value: T) => {
+        resolve(value);
+      });
+      this.rejectListeners.push((error: unknown) => {
+        reject(error);
+      });
+    } else if (this.result.tag === 'error') {
+      reject(this.result.error);
+    } else {
+      resolve(this.result.value);
+    }
+  }
+
+  private resolveAndAnnounce(value: T) {
+    this.result = { tag: 'value', value };
+
+    for (const listener of this.resolveListeners) {
+      listener(value);
+    }
+  }
+
+  private rejectAndAnnounce(error: unknown) {
+    this.result = { tag: 'error', error };
+
+    for (const listener of this.rejectListeners) {
+      listener(error);
+    }
+  }
+
+  // Unused - just showing a "unified" setter
+  private setResult<R extends PromiseResult<T>>(result: R) {
+    this.result = result;
+
+    // How sad these casts are needed :'(
+
+    const listeners = (
+      result.tag === 'value' ? this.resolveListeners : this.rejectListeners
+    ) as ((x: R['tag'] extends 'value' ? T : unknown) => void)[];
+
+    const x = (
+      result.tag === 'value' ? result.value : result.error
+    ) as R['tag'] extends 'value' ? T : unknown;
+
+    for (const listener of listeners) {
+      listener(x);
+    }
   }
 }
