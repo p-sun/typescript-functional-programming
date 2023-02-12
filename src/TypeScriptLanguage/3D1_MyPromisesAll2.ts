14e84c533bd8f426ba1ed97c61071891650e56f9
Paige Sun <paige.sun.dev@gmail.com>
2023-02-11 23:28:31 -0800

    comments: Add comments tracing how to recursively bubble up an error in MyPromisesAll2.ts.

--- src/TypeScriptLanguage/3D1_MyPromisesAll2.ts
+++ src/TypeScriptLanguage/3D1_MyPromisesAll2.ts
@@ -2,21 +2,31 @@
 /*                        Recursively bubble up error                         */
 /* -------------------------------------------------------------------------- */
 
-// Similar to MyPromisesResult in 3C1_MyPromisesAll.ts
-// except it returns [] if ANY element is NOT a Promise.
+// Similar to MyPromisesResult in 3C1_MyPromisesAll.ts, except it
+// returns [] if ANY element is NOT a Promise.
 export type MyPromisesResult2<PArray extends readonly any[]> =
   _PromisesResult<PArray> extends 'NotAPromise' ? [] : _PromisesResult<PArray>;
 
-// Need 'NotAPromise' to distinguish between _PromisesResult<[]> and _PromisesResult<'NotAPromise'>
-type _PromisesResult<PArray extends readonly any[] | 'NotAPromise'> =
-  PArray extends []
-    ? []
-    : PArray extends readonly [infer Head, ...infer Tail] // PArray is a readonly tuple.
-    ? Head extends Promise<infer H> // Head EXISTS, Tail may be [].
-      ? _PromisesResult<Tail> extends 'NotAPromise'
-        ? 'NotAPromise' // Propogate ERROR to result: If any element in Tail is NOT a Promise, reduce to [].
-        : [H, ..._PromisesResult<Tail>] // Unwrap Head, recurse through Tail.
-      : 'NotAPromise' // Encounter ERROR: Head is NOT a Promise.
-    : PArray extends Promise<infer Element>[] // PArray is a mutable Array.
-    ? Element[] // Unwrap Array.
-    : []; // Not possible b/c PArray extends readonly any[]
+// Returns 'NotAPromise' if ANY element is NOT a Promise.
+// We need 'NotAPromise' to distinguish between
+// _PromisesResult<[]> and _PromisesResult<'NotAPromise'>.
+//
+// The numbers represent the order of execution:
+// 1 _PromisesResult<[Promise<number>, string]>
+// 4 _PromisesResult<[string]>
+type _PromisesResult<PArray extends readonly any[]> = PArray extends []
+  ? []
+  : PArray extends readonly [infer Head, ...infer Tail]
+  ? // 2 Head = Promise<number>, Tail = [string]
+    // 5 Head = string, Tail = []
+    Head extends Promise<infer H>
+    ? // 3 H = number, _PromisesResult<Tail> = _PromisesResult<[string]>
+      _PromisesResult<Tail> extends 'NotAPromise'
+      ? // 7 return 'NotAPromise' to 1 _PromisesResult<[Promise<number>, string]>
+        'NotAPromise'
+      : [H, ..._PromisesResult<Tail>]
+    : // 6 return 'NotAPromise' to 3 _PromisesResult<[string]>
+      'NotAPromise'
+  : PArray extends Promise<infer Element>[]
+  ? Element[]
+  : [];
