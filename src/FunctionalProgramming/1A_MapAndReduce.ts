074364a96dc3a63ab99a3f068004071a8cd8717c
Paige Sun <paige.sun.dev@gmail.com>
2023-02-15 17:40:16 -0800

    add: Add TS examples for implementing Map and Reduce from scratch.

---
+++ src/FunctionalProgramming/1A_MapAndReduce.ts
@@ -0,0 +1,51 @@
+/* -------------------------------------------------------------------------- */
+/*                  Make a sandwhich with `map` and `reduce`                  */
+/* -------------------------------------------------------------------------- */
+
+// Step 1: Start with an array of ingredients.
+const items = ['🥬', '🍅', '🧀'];
+
+// Step 2: Slice all the items.
+// Use `map` to perform an action on every item.
+// ['sliced 🥬', 'sliced 🍅', 'sliced 🧀'];
+const slicedItems = items.map((item) => `sliced ${item}`);
+
+// Step 3: Assemble a sandwhich.
+// Use `reduce` to combine multiple items into one item.
+// 'MY SANDWHICH: sliced 🥬 | sliced 🍅 | sliced 🧀 |'
+const sandwhich = slicedItems.reduce(
+  (sandwhich, item) => `${sandwhich} ${item} | `,
+  'MY SANDWHICH: '
+);
+
+/* -------------------------------------------------------------------------- */
+/*                  Implement `map` and `reduce` from scratch                 */
+/* -------------------------------------------------------------------------- */
+
+function arrayMap<T, S>(list: S[], fn: (n: S) => T): T[] {
+  const result: T[] = [];
+  for (const n of list) {
+    result.push(fn(n));
+  }
+
+  return result;
+}
+
+// ['num: 1', 'num: 2', 'num: 3']
+const m = arrayMap([1, 2, 3], (n) => 'num: ' + n.toString());
+
+function arrayReduce<T, S>(
+  list: T[],
+  fn: (acc: S, element: T) => S,
+  initialValue: S
+): S {
+  let result = initialValue;
+  for (const element of list) {
+    result = fn(result, element);
+  }
+
+  return result;
+}
+
+// '-1-2-3'
+const n = arrayReduce([1, 2, 3], (acc, x) => acc + '-' + x.toString(), '');
