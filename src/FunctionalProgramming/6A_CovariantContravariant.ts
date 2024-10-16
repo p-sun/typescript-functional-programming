9e1008480410cc0f4ef7b91f63653c6760e0d49d
Paige Sun <paige.sun.dev@gmail.com>
2024-10-16 17:20:09 -0400

    update: Cleanup notes on covariance and contravariance on unary type constructors.

--- src/FunctionalProgramming/6A_CovariantContravariant.ts
+++ src/FunctionalProgramming/6A_CovariantContravariant.ts
@@ -1,9 +1,9 @@
-type IsSubtype<T, U> = T extends U ? true : false;
-type CheckSubtype<A, B> = A extends B ? (B extends A ? 'both' : 'subtype') : (B extends A ? 'supertype' : 'none')
-
 /* -------------------------------------------------------------------------- */
 /*                            Basic Type Inference                            */
 /* -------------------------------------------------------------------------- */
+type CheckSubtype<A, B> = A extends B ? 
+    (B extends A ? 'both' : 'subtype') : 
+    (B extends A ? 'supertype' : 'none')
 
 const b0: CheckSubtype<number, number> = 'both'
 const b1: CheckSubtype<string, number> = 'none'
@@ -18,7 +18,7 @@ class Empty2 {}
 
 // TS uses structural subtyping, not the name of the type or class.
 // This means it compares the values and functions in the type.
-const e0: IsSubtype<Empty1, Empty2> = true
+const e0: CheckSubtype<Empty1, Empty2> = 'both'
 
 class Parent {
     parentFn(): string { return ''}
@@ -28,95 +28,79 @@ class Child extends Parent {
     childFn(): number { return 1 }
 }
 
-const c1: IsSubtype<Child, Parent> = true
-
-/* -------------------------------------------------------------------------- */
-/*                            Arrays and Subtyping                            */
-/* -------------------------------------------------------------------------- */
-
-const b4: CheckSubtype<Child[], Parent[]> = 'subtype'
-
-// TS will allow you to pass a subtype into a function expecting supertype.
-// However! TS can allow you to illegal things!
-
-// Theoretically can I pass a Child anywhere that expect a Parent? YES
-// Theoretically can I pass a Parent anywhere that expect a Child? NO
-
-// Theoretically, can I pass a Child[] to someone who expects a Parent[]?
-function takeParents(arr: Parent[]) {
-    arr[0].parentFn() // If you only read it... ok!
-    arr.push(new Parent()) // If you mutate it... no! If we push a Parent, it's unsafe! TS doesn't warn us.
-}
-const childArray: Child[] = []
-takeParents(childArray)
-childArray[0].childFn() // ERROR: childFn() is undefined
-
-// Theoretically, can I pass a Parent[] to something that expect a Child[]?
-function takeChildren(arr: Child[]) {
-    arr[0].childFn() // If you read it... no!
-    arr.push(new Child()) // If you only mutate it... ok!
-}
-const parentsArray: Parent[] = []
-// takeChildren(parentsArray) // ERROR: 'Parent[]' is not assignable to parameter of type 'Child[]'
-parentsArray[0].parentFn()
-
-// Can I pass a (c: Child) => string to a (c: Parent) => string
-// NO! We might end up call a Child function on a Parent instance
-// Can I pass a (c: Parent) => string to a (c: Child) => string
-// YES!
-
-// Summary:
-// Child < Parent
-// Covariant: ImmutableArray<Child> < ImmutableArray<Parent>
-// Contravariant: (Parent => T) < (Child => T)
+const c1: CheckSubtype<Child, Parent> = 'subtype'
 
 /* -------------------------------------------------------------------------- */
 /*                      Covariant vs Contravariant Types                      */
 /* -------------------------------------------------------------------------- */
 /**
- * IsSubtype<T<Child>, T<Parent>> is true, T is covariant. i.e. The Child is on the left.
- * IsSubtype<T<Parent>, T<Child>> is true, T is contravariant. i.e. The Child is on the right.
- * To make types clearer, all remaining IsSubtype in this file are assigned to true.
+  Let the relation "isSubtype of" be denoted by `<`. 
+
+  Covariant types have Child on the left of the `<` relation:
+    Child < Parent
+    (String => Child) < (String => Parent)
+    ImmutableArray<Child> < ImmutableArray<Parent>
+
+  Contravariant types have Parent on the left of the `<` relation:
+    (Parent => String) < (Child => String)
+
+  Given (immutable) unary type constructor X<T>,
+    If CheckSubtype<X<Child>, X<Parent>> is 'subtype', X is covariant.
+    If CheckSubtype<X<Child>, X<Parent>> is 'supertype', X is contravariant.
  */
 
 /* -------------------------- Identity is covariant ------------------------- */
 
 type Identity<T> = T
-const f0: IsSubtype<Identity<Child>, Identity<Parent>> = true
+const c3: CheckSubtype<Identity<Child>, Identity<Parent>> = 'subtype'
 
-// T is Covariant when T<Child> extends T<Parent>. Meaning you can pass T<Child> to a function expecting T<Parent>.
-function covariantFn(x: Identity<Parent>) {
-    x.parentFn()
+// T is Covariant when T<Child> extends T<Parent>.
+// Meaning you can pass T<Child> in a function expecting T<Parent>.
+function c3_(parent: Identity<Parent>) {
+    parent.parentFn()
 }
-covariantFn(new Child())
+c3_(new Child() as Identity<Child>)
 
-/* ----------------------- ArrowRight is covariant ----------------------- */
+/* ----------------------- Return Types are covariant ----------------------- */
 
-type ArrowRight<T> = (s: string) => T
-const c5: IsSubtype<(x: string) => Child, (x: string) => Parent> = true
+const c5: CheckSubtype<(x: string) => Child, (x: string) => Parent> = 'subtype'
 
 function c5_(toParent: (s: string) => Parent) {
     toParent('hello').parentFn()
 }
-const toChild = (_: string) => new Child()
-c5_(toChild)
+c5_((_: string) => new Child() )
 
-/* ----------------------- ArrowLeft is contravariant ----------------------- */
+/* --------------------- Parameter Types are contravariant -------------------- */
 
-type ArrowLeft<T> = (x: T) => string
-const c4: IsSubtype<(x: Parent) => string, (x: Child) => string> = true
+const c4: CheckSubtype<(x: Child) => string, (x: Parent) => string> = 'supertype'
 
-// T is Contravariant when T<Parent> extends T<Child>. Meaning you can pass T<Parent> to a function expecting T<Child>.
-function contravariantFn(takeChild: (x: Child) => string) {
+// T is Contravariant when T<Parent> extends T<Child>.
+// Meaning you can pass T<Parent> to a function expecting T<Child>.
+function c4_(takeChild: (x: Child) => string) {
     takeChild(new Child())
 }
-const takeParent = (x: Parent) => x.parentFn()
-contravariantFn(takeParent)
+c4_((x: Parent) => x.parentFn())
+
+/* ---------------------- Immutable Arrays are covariant ---------------------- */
+
+const c2: CheckSubtype<Child[], Parent[]> = 'subtype'
+
+// TS will allow you to pass a subtype into a function expecting supertype.
+// However! TS can allow you to illegal things!
+function c2_(arr: Parent[]) {
+    arr[0].parentFn() // If you only read from the array, it's okay because Parent's methods are accessible.
+    arr.push(new Parent()) // But if you mutate it by adding a Parent, it becomes unsafe for Child arrays. 
+    // TypeScript doesn't warn us here, which could lead to runtime errors.
+}
+
+const childArray: Child[] = [new Child()]
+c2_(childArray)
+childArray[1].childFn() // ERROR: childFn() is undefined because a Parent was added to the Child array.
 
 /* -------------------- Pair is a co-co variant bifunctor -------------------- */
 
 type Pair<A, B> = [A, B]
-const c6: IsSubtype<Pair<Child, Child>, Pair<Parent, Parent>> = true
+const c6: CheckSubtype<Pair<Child, Child>, Pair<Parent, Parent>> = 'subtype'
 
 // We can also have co-contra, contra-co, and contra-contra bifunctors
 
@@ -124,13 +108,13 @@ type Co<T> = T
 type Contra<T> = (x: T) => string
 
 type ContraContra<A, B> = [Contra<A>, Contra<B>]
-const c7: IsSubtype<ContraContra<Parent, Parent>, ContraContra<Child, Child>> = true
+const c7: CheckSubtype<ContraContra<Child, Child>, ContraContra<Parent, Parent>> = 'supertype'
 
 type CoContra<A, B> = [Co<A>, Contra<B>]
-const c8: IsSubtype<CoContra<Child, Parent>, CoContra<Parent, Child>> = true
+const c8: CheckSubtype<CoContra<Child, Parent>, CoContra<Parent, Child>> = 'subtype'
 
 type ContraCo<A, B> = [Contra<A>, Co<B>]
-const c9: IsSubtype<ContraCo<Parent, Child>, ContraCo<Child, Parent>> = true
+const c9: CheckSubtype<ContraCo<Child, Parent>, ContraCo<Parent, Child>> = 'supertype'
 
 /* --------------- Nesting Co and Contra variant functor types -------------- */
 // Wrapping a type in Co doesn't change its variance.
@@ -143,10 +127,10 @@ type Contra1<T> = Contra<Contra<Contra<T>>>
 type Contra2<T> = Co<Contra<T>>
 type Contra3<T> = Contra<Co<T>>
 
-const c14: IsSubtype<Contra1<Parent>, Contra1<Child>> = true
-const c10: IsSubtype<Contra3<Parent>, Contra3<Child>> = true
-const c11: IsSubtype<Contra2<Parent>, Contra2<Child>> = true
-const c12: IsSubtype<Co2<Child>, Co2<Parent>> = true
-const c13: IsSubtype<Co3<Child>, Co3<Parent>> = true
+const c14: CheckSubtype<Contra1<Child>, Contra1<Parent>> = 'supertype'
+const c10: CheckSubtype<Contra3<Child>, Contra3<Parent>> = 'supertype'
+const c11: CheckSubtype<Contra2<Child>, Contra2<Parent>> = 'supertype'
+const c12: CheckSubtype<Co2<Child>, Co2<Parent>> = 'subtype'
+const c13: CheckSubtype<Co3<Child>, Co3<Parent>> = 'subtype'
 
 export default function run() { }
